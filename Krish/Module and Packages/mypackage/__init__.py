e96ea7bdfe1991d5fb236d4c1a60412dc4d4df54
connectoshivam <connecttoshivam1@gmail.com>
2025-11-01 00:10:40 +0530

    Learnings

--- Krish/Module and Packages/mypackage/__init__.py
+++ Krish/Module and Packages/mypackage/__init__.py
@@ -1,1 +1,8 @@
-''' we are create a module to understand the concepts of pakeges and module'''
+# Inside mypackage/__init__.py
+
+from .module1 import sums
+from .module2 import mul
+from .one import add
+
+# Expose them as part of the package namespace
+__all__ = ['add', 'mul', 'sums']
